f65dc9939e0e887339e5d5afc2b202f6b13ce067
Victor Prado <vicpradochaves@gmail.com>
2025-06-08 19:20:48 -0300

    alterado o texto do arquivo program para testar a automação

--- .github/workflows/Program.cs
+++ .github/workflows/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Mudança no meu código para verficar comportamento a partir da mudança");
